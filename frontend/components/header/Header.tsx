f14aac5c46bb14b4be119a053733579baafd86ec
ENZO EMIR VIANA FERRAZ <164296530+EnzoEmir@users.noreply.github.com>
2026-05-05 01:02:10 -0300

    fix: header banana
    Recoloquei o header amarelo pq o Caio gostava
    Me perdoem todos :(

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -4,7 +4,7 @@ import MobileMenuButton from "./MobileMenuButton";
 
 export default function Header(){
     return(
-        <header className="header-white sticky top-0 z-50 bg-black border-b border-gray-800">
+        <header className="sticky top-0 z-50 backdrop-blur bg-yellow-400">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center md:justify-start md:gap-10">
                     <Logo />
